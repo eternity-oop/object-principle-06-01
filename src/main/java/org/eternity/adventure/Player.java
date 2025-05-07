15378e069fe1050c13b31e78c684027d7d759a12
조영호 <aeternum9342@gmail.com>
2025-05-07 22:20:02 +0900

    오브젝트 설계 원칙편 6-1 예제

---
+++ src/main/java/org/eternity/adventure/Player.java
@@ -0,0 +1,28 @@
+package org.eternity.adventure;
+
+public class Player {
+    private WorldMap worldMap;
+    private Position position;
+
+    public Player(WorldMap worldMap, Position position) {
+        this.worldMap = worldMap;
+        this.position = position;
+    }
+
+    public boolean move(Direction direction) {
+        if (worldMap.isBlocked(position.shift(direction))) {
+            return false;
+        }
+
+        this.position = this.position.shift(direction);
+        return true;
+    }
+
+    Position position() {
+        return position;
+    }
+
+    public Room currentRoom() {
+        return worldMap.roomAt(position);
+    }
+}
---
+++ src/test/java/org/eternity/adventure/PlayerTest.java
@@ -0,0 +1,72 @@
+package org.eternity.adventure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest {
+    @Test
+    public void move_east() {
+        WorldMap worldMap = new WorldMap(
+                Size.with(2, 2),
+                new Room(Position.of(0, 0), "", ""),
+                new Room(Position.of(1, 0), "", ""),
+                new Room(Position.of(1, 1), "", ""));
+        Player player = new Player(worldMap, Position.of(0, 0));
+
+        assertThat(player.move(Direction.EAST)).isTrue();
+        assertThat(player.position()).isEqualTo(Position.of(1, 0));
+    }
+
+    @Test
+    public void can_not_move_west() {
+        WorldMap worldMap = new WorldMap(
+                Size.with(2, 2),
+                new Room(Position.of(0, 0), "", ""),
+                new Room(Position.of(1, 0), "", ""),
+                new Room(Position.of(1, 1), "", ""));
+        Player player = new Player(worldMap, Position.of(1, 1));
+
+        assertThat(player.move(Direction.WEST)).isFalse();
+        assertThat(player.position()).isEqualTo(Position.of(1, 1));
+    }
+
+    @Test
+    public void can_not_move_outside() {
+        WorldMap worldMap = new WorldMap(
+                Size.with(2, 2),
+                new Room(Position.of(0, 0), "", ""),
+                new Room(Position.of(1, 0), "", ""),
+                new Room(Position.of(1, 1), "", ""));
+        Player player = new Player(worldMap, Position.of(1, 1));
+
+        assertThat(player.move(Direction.EAST)).isFalse();
+        assertThat(player.position()).isEqualTo(Position.of(1, 1));
+    }
+
+    @Test
+    public void move_throws_exception() {
+        WorldMap worldMap = new WorldMap(
+                Size.with(2, 2),
+                new Room(Position.of(0, 0), "", ""),
+                new Room(Position.of(1, 0), "", ""),
+                new Room(Position.of(1, 1), "", ""));
+        Player player = new Player(worldMap, Position.of(1, 1));
+
+        assertThat(player.move(Direction.EAST)).isFalse();
+    }
+
+    @Test
+    public void move_passed() {
+        WorldMap worldMap = new WorldMap(
+                Size.with(2, 2),
+                new Room(Position.of(0, 0), "", ""),
+                new Room(Position.of(1, 0), "", ""),
+                new Room(Position.of(1, 1), "", ""));
+        Player player = new Player(worldMap, Position.of(0, 0));
+
+        player.move(Direction.EAST);
+
+        assertThat(player.position()).isEqualTo(Position.of(1, 0));
+    }
+}
